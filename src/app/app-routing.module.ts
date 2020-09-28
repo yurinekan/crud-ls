39a702d2e581792c9e58b937d5c1bdeb66ebddc9
Yuri Nekan <yurinekanfts@hotmail.com>
2020-09-28 03:46:05 -0300

    Home remove

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,10 +8,6 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
-  },
-  {
     path: 'list',
     loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
   },
